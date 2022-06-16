186ab168bbe7e6d2eb4c281971648ef5ab65a130
Mariame12 <mariameconte1234@email.com>
2022-06-16 23:15:00 +0000

    version soir

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+
+    //public $apilink="https://api.oumardev.com/apoloanapi/";
+    function list(){
+        
+        $response=Http::withHeaders(['Authorization' =>"Bear ".$_COOKIE['token']])->get("http://www.oumardev.com:5400/apoloanapi/user")->json();
+        echo var_dump($response);
+    }
+
+    public function index()
+    {
+        return view('Presentation.accueil');
+    }  
+   
+    public function login()
+    {
+        return view('Utilisateurs.login');
+
+    } 
+    public function register()
+    {
+        return view('Utilisateurs.register');
+
+    } 
+ 
+    public function Refil( Request $request){
+        
+        return view('Utilisateurs.refil');
+    }
+
+    public function Registersave(Request $request){
+        $this->validate($request, [
+            'nom'=>'regex:/^[a-zA-Z ]+$/',
+            'prenom' => 'regex:/^[a-zA-Z ]+$/',
+            'numero'=> 'required',
+            'password' => 'required',
+            
+        ]);
+        $response=Http::withHeaders(['Authorization' =>"Bear ".$_COOKIE['token']])->post('http://www.oumardev.com:5400/apoloanapi/register', [
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'numero' =>intval( $request->numero),
+            'password' => $request->password,
+        ])->json();
+        
+        echo var_dump($response);
+
+        
+    }
+
+    public function LoginSave( Request $request)
+    {
+        $response = Http::post('http://www.oumardev.com:5400/apoloanapi/login', [
+
+            'numero' =>intval( $request->numero),
+           'password' => $request->password,
+         ])->json();
+        
+        if(isset($response["error"])){
+            
+            return redirect()->route('login')->with('error',$response["error"]);
+            
+        }
+        else{
+            $cookie_name = "token";
+            $cookie_value = $response["token"];
+            setcookie($cookie_name, $cookie_value, time()+ (86400), "/");
+
+          $response=Http::withHeaders(['Authorization' =>"Bear $cookie_value"])->post("http://www.oumardev.com:5400/apoloanapi/login");
+          return view('Presentation/menu');
+         // $_COOKIE['token']
+            
+        }
+        
+    }
+    
+}
---
+++ resources/views/Utilisateurs/login.blade.php
@@ -0,0 +1,107 @@
+
+<html>
+
+<head>
+    <title>Login Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/LOGIN.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <header class="header" id="top">
+        <div class="w3-top">
+            <div class="w3-bar  w3-card header">
+
+                <span style="COLOR: WHITE;MARGIN-LEFT:37REM" class=" logo  w3-padding-large ">APOLOAN</span>
+
+            </div>
+        </div>
+
+
+
+    </header>
+    <div class="login-wrap">
+        <div class="az">
+            <div class="login-html">
+                <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2"
+                    class="tab w3-btn w3-round-xlarge ">S'inscrire</label>
+                <div class="login-form">
+                
+                    <form method="POST" action="{{ route('save.login') }}">
+                        @csrf
+
+                        <div class="sign-up-htm">
+                            @if(session('error'))
+                            <div style="color:red; text-align:center;" class="alert alert-danger">
+                                    {{session('error')}}
+                            </div>
+                         @endif
+                            <div class="group">
+                                <label for="user" class="label">Numero</label>
+                                <input id="user" name="numero" type="text" class="input">
+                            </div>
+                            <div class="group">
+                                <label for="pass" class="label">Mot de passe</label>
+                                <input id="pass" type="password" class="input" name="password"
+                                    data-type="password">
+                            </div>
+                            <div class="group">
+                                <input type="submit" class="button" value="Valider">
+                            </div>
+                            <div class="hr"></div>
+                            <div class="foot-lnk">
+                                <label class="label-foo w3-btn w3-round-xlarge " for="tab-2" class="label-foo">Vous
+                                    n’avez pas de compte&nbsp;? Inscrivez-vous!</label>
+                            </div>
+                        </div>
+                    </form>
+                    <form method="POST" action="{{ route('save.register') }}">
+                        @csrf
+
+                        <div class="sign-up-htm">
+                            <div class="group">
+                            @if(session('error'))
+                                <div style="color:red; text-align:center;" class="alert alert-danger">
+                                        {{session('error')}}
+                                </div>
+                          @endif
+                                <label class="label">Nom</label>
+                                <input type="text" name="nom" class="input">
+                            </div>
+                            <div class="group">
+                                <label class="label">Prenom</label>
+                                <input type="text" name="prenom" class="input">
+                            </div>
+                            <div class="group">
+                                <label class="label"> Numero</label>
+                                <input type="text" name="numero" class="input">
+                            </div>
+                            <div class="group">
+                                <label class="label">Password</label>
+                                <input type="password" name="password" class="input" data-type="password">
+                            </div>
+
+                            <div class="group">
+                                <input type="submit" class="button" value="Envoyer">
+                            </div>
+                            <div class="hr-2"></div>
+                            <div class="foot-lnk">
+                                <label class="label-foo-2 w3-btn w3-round-xlarge " for="tab-1">Vous
+                                    n’avez pas de compte&nbsp;? Inscrivez-vous!</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
